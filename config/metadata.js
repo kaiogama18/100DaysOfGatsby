befe9c9fd163f93f301fbbde84faf70ea28840d6
Kaio B. Gama <kaioeduardoescar@gmail.com>
2021-02-06 20:04:40 -0400

    Use Chakra and create page contact 2
    Signed-off-by: Kaio B. Gama <kaioeduardoescar@gmail.com>

--- config/metadata.js
+++ config/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
     title: "challenge-gatsby",
+    siteUrl: `https://www.challengegatsby.com`,
     titleTemplate: "%s · 100 days",
     description: `#100DaysOfGatsby, Gatsby’s guided coding challenge for 2021!`,
-    url: "https://www.challengegatsby.com", 
     image: "/images/icon.jpg", 
     twitterUsername: "@kaioedu18",
 }
